1e2481c997c579e5c80fd535356226b82ca3f8e3
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-06-08 23:04:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,13 @@ Nihaal=Client(
     api_id="18850696",
     api_hash="e3f5a7b4ea28b5c7ce9fc65e48274ae6"
 )
+START_MESSAGE="""Hey {} How are you buddy"""
 
 @Nihaal.on_message(filters.command("start"))
 async def start_message(Nihaal,message):
     await message.reply_photo(
         photo="https://telegra.ph/file/3f5c3a461d41522a3b7d2.jpg",
-        caption="➪ ʜᴇʏ ʙᴜᴅᴅʏ ɪᴀᴍ sɪᴍᴘʟᴇ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ ✌︎",
+        caption=START_MESSAGE.fromat(message.from_user.mention)
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/NL_MP4"),
             InlineKeyboardButton("ɢʀᴏᴜᴘ", url="https://t.me/movie_lookam")
