e1724a56180fc3bec3cb70bcc2494aa6c1975b8f
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-06-08 18:44:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from pyrogram import client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-nihaal=client(
+Nihaal=client(
     "Pyrogram Bot",
     bot_token="5541342240:AAFvlATqafF1FB7TtMAMNVqdOv04c1a7nuY",
     api_id="18850696",
     api_hash="e3f5a7b4ea28b5c7ce9fc65e48274ae6"
 )
 
-@nihaal.on_message(filters.command("start"))
-async def start_message(nihaal,message):
+@Nihaal.on_message(filters.command("start"))
+async def start_message(Nihaal,message):
     await message.reply_photo(
         photo="https://telegra.ph/file/3f5c3a461d41522a3b7d2.jpg",
         caption="hello iam just a test bot ",
