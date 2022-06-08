af96206b1b743bc56128501321bfaba73b327298
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-06-08 18:40:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from pyrogram import client, filters
-from pyrogram.type import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 nihaal=client(
     "Pyrogram Bot",
