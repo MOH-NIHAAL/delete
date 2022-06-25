ad93d6ae1258d8853b51cab1446a2d170d386e59
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-06-25 13:55:54 +0530

    Update pm_filter.py

--- NL_BOTZ/pm_filter.py
+++ NL_BOTZ/pm_filter.py
@@ -5,7 +5,7 @@ import ast
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
-from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
+from DB.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
     SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
@@ -13,9 +13,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, split_list, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
-from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_filter_results
-from database.filters_mdb import (
+from DB.users_chats_db import db
+from DB.ia_filterdb import Media, get_file_details, get_filter_results
+from DB.filters_mdb import (
     del_all,
     find_filter,
     get_filters,
