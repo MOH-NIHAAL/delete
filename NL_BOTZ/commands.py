4a499925ec822ea7a42c67a4c941b1bd74b283f5
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-06-27 22:29:16 +0530

    Update commands.py

--- NL_BOTZ/commands.py
+++ NL_BOTZ/commands.py
@@ -46,7 +46,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-        InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.B_NAME}?startgroup=true')
+        InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/Srinidhi_shetty_bot?startgroup=true')
         ], [
         InlineKeyboardButton('⚙️ ʜᴇʟᴘ', callback_data='help'),
         InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about_menu')
@@ -91,12 +91,12 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-        InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.B_NAME}?startgroup=true')
+        InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/Srinidhi_shetty_bot?startgroup=true')
         ], [
         InlineKeyboardButton('⚙️ ʜᴇʟᴘ', callback_data='help'),
         InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about_menu')
         ], [
-        InlineKeyboardButton("🔖 sᴜᴘᴘᴏʀᴛ", url='https://t.me/NL_BOTxCHAT')
+        InlineKeyboardButton("🔖 sᴜᴘᴘᴏʀᴛ 🕊️", url='https://t.me/NL_BOTxCHAT')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
